2ddd1b5dda3f9471fcd9eae068c99dbdbfa87ab3
Mikhail Materukhin <9226343456@mail.ru>
2019-06-18 11:13:11 +0500

    Initial commit
    создание точки входа в программу

---
+++ src/main/java/com/github/muhin007/iWeatherProvider/Main.java
@@ -0,0 +1,7 @@
+package com.github.muhin007.iWeatherProvider;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Средняя температура в городе " + "nameCity" + " : " + "avgCurrentTemperature" + " градусов.");
+    }
+}
