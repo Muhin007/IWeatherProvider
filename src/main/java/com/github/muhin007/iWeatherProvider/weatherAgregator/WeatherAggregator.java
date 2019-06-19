b2bc775f2815e8148b88a43c3a7b392fcc367055
Mikhail Materukhin <9226343456@mail.ru>
2019-06-19 22:54:52 +0500

    wp-2 Получаем температуру от WeatherAdaptorYandex

--- src/main/java/com/github/muhin007/iWeatherProvider/weatherAgregator/WeatherAggregator.java
+++ src/main/java/com/github/muhin007/iWeatherProvider/weatherAgregator/WeatherAggregator.java
@@ -1,12 +1,15 @@
 package com.github.muhin007.iWeatherProvider.weatherAgregator;
 
+import com.github.muhin007.iWeatherProvider.weatherAdaptor.yandex.WeatherAdaptorYandex;
+
 import java.util.Scanner;
 
 public class WeatherAggregator {
 
     private static volatile WeatherAggregator instance = null;
+    WeatherAdaptorYandex wAY = new WeatherAdaptorYandex();
     private String cityName = null;
-    private double avgTemp, yandexTemp = 0, accuweatherTemp = 0, aerisweatherTemp = 0, worldweatheronlineTemp = 0;
+    private double avgTemp, yandexTemp = wAY.getTempYandex(), accuweatherTemp = 0, aerisweatherTemp = 0, worldweatheronlineTemp = 0;
 
     public static WeatherAggregator getInstance() {
         if (instance == null) {
@@ -20,11 +23,10 @@ public class WeatherAggregator {
     }
 
     public void start(){
-
         System.out.println("Средняя температура в городе " + getCityName() + " : " + getAVGTemp() + "C.");
     }
 
-    private String getCityName(){
+    public String getCityName(){
         Scanner in = new Scanner(System.in);
         System.out.print("Введите название города: ");
         cityName = in.nextLine();
