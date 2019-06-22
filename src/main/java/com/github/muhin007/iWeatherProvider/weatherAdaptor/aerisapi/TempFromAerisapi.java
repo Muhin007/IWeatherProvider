6513cd298ee56f92ba926bbe94ad32398e8dffbc
Mikhail Materukhin <9226343456@mail.ru>
2019-06-23 01:37:16 +0500

    wp-4 Берем координаты города

--- src/main/java/com/github/muhin007/iWeatherProvider/weatherAdaptor/aerisapi/TempFromAerisapi.java
+++ src/main/java/com/github/muhin007/iWeatherProvider/weatherAdaptor/aerisapi/TempFromAerisapi.java
@@ -1,6 +1,8 @@
 package com.github.muhin007.iWeatherProvider.weatherAdaptor.aerisapi;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.muhin007.iWeatherProvider.weatherAdaptor.JSONReadProcess;
+import com.github.muhin007.iWeatherProvider.weatherAdaptor.aerisapi.JSONClass.Loc;
 import com.github.muhin007.iWeatherProvider.weatherAdaptor.aerisapi.JSONClass.Period;
 import com.github.muhin007.iWeatherProvider.weatherAdaptor.aerisapi.JSONClass.Response;
 import com.github.muhin007.iWeatherProvider.weatherAdaptor.aerisapi.JSONClass.Temp;
@@ -16,7 +18,16 @@ import java.net.URL;
 public class TempFromAerisapi {
     private static String city = WeatherAggregator.getCityName();
     private int tempFromAerisapi;
+    private volatile double _long;
+    private volatile double lat;
 
+    public double get_long() {
+        return _long;
+    }
+
+    public double getLat() {
+        return lat;
+    }
     public int getTempFromAerisapi() {
         parsingTempFromAerisapi();
         return tempFromAerisapi;
@@ -24,7 +35,7 @@ public class TempFromAerisapi {
 
     private void parsingTempFromAerisapi() {
         String request = "https://api.aerisapi.com/forecasts/" + city + ",?format=json&filter=day&limit=1&client_id=a04yWLdF5v83ZGGqanosb&client_secret=2moGBj8LBRIlJdhaufg9qCxN7IPLCokJ3OjamuKK";
-        String result = performRequest(request);
+        String result = JSONReadProcess.performRequest(request);
         ObjectMapper mapper = new ObjectMapper();
         Temp temp = null;
         try {
@@ -33,41 +44,11 @@ public class TempFromAerisapi {
             e.printStackTrace();
         }
         for (Response response : temp.getResponse()) {
+            _long = response.getLoc().getLong();
+            lat = response.getLoc().getLat();
             for (Period period : response.getPeriods()) {
                 tempFromAerisapi = (int) period.getMaxTempC();
             }
         }
-    }
-
-    private static String performRequest(String urlStr) {
-        URL url = null;
-        try {
-            url = new URL(urlStr);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-        StringBuilder sb = new StringBuilder();
-
-        HttpURLConnection http = null;
-        try {
-            http = (HttpURLConnection) url.openConnection();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try {
-            BufferedReader br = new BufferedReader(new InputStreamReader(http.getInputStream()));
-            char[] buf = new char[1000000];
-
-            int r = 0;
-            do {
-                if ((r = br.read(buf)) > 0)
-                    sb.append(new String(buf, 0, r));
-            } while (r > 0);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            http.disconnect();
-        }
-        return sb.toString();
     }
 }
