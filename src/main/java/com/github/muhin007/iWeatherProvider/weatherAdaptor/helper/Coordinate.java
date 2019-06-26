9ff56cf5f7774eddbaddc8f2d5c10b87c6234901
Mikhail Materukhin <9226343456@mail.ru>
2019-06-26 14:22:10 +0500

    wp-7 вынесен метод для получения координат

---
+++ src/main/java/com/github/muhin007/iWeatherProvider/weatherAdaptor/helper/Coordinate.java
@@ -0,0 +1,28 @@
+package com.github.muhin007.iWeatherProvider.weatherAdaptor.helper;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.muhin007.iWeatherProvider.weatherAdaptor.aerisapi.JSONClass.Response;
+import com.github.muhin007.iWeatherProvider.weatherAdaptor.aerisapi.JSONClass.Temp;
+
+import java.io.IOException;
+
+public class Coordinate {
+
+    public void getCoordinate(String cityName) {
+        String request = "https://api.aerisapi.com/forecasts/" + cityName + ",?format=json&filter=day&limit=1&client_id=a04yWLdF5v83ZGGqanosb&client_secret=2moGBj8LBRIlJdhaufg9qCxN7IPLCokJ3OjamuKK";
+        String result = JSONReadProcess.performRequest(request);
+        ObjectMapper mapper = new ObjectMapper();
+        Temp temp = null;
+        try {
+            temp = mapper.readValue(result, Temp.class);
+        } catch (IOException e) {
+            System.out.println("ошибка чтения данных сайта");
+            e.printStackTrace();
+        }
+        assert temp != null;
+        for (Response response : temp.getResponse()) {
+            double lon = response.getLoc().getLong();
+            double lat = response.getLoc().getLat();
+        }
+    }
+}
